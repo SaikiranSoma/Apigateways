ebe04dd8a83ebf19a8241962d64b4d6af99f534f
Saikiran Soma <Saikiran.Soma@ust.com>
2024-08-05 09:21:57 +0530

    Add project files.

---
+++ OrdersApi/Controllers/OrderController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrdersApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+
+
+        public IActionResult GetOrders()
+        {
+            return Ok("Your orders");
+        }
+    }
+}
---
+++ Products/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+
+        public IActionResult GetProduct()
+        {
+            return Ok("Products");
+        }
+    }
+}
